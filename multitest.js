4c09c0a51ac6a36cf17827ec7bb74c4c2ee0e776
Javier Collado <javier.collado@gmail.com>
2015-12-23 14:59:56 +0100

    Move main functionality to a main function in the library
    This way the script is kept very small and all the functionality is
    under the `lib` directory

--- multitest.js
+++ multitest.js
@@ -1,20 +1,8 @@
 #!/usr/bin/env node
 'use strict'
 
-const logger = require('./lib/logging').logger
-const parseArguments = require('./lib/arguments').parseArguments
+const main = require('./lib/main')
 
 if (require.main === module) {
-  const pkg = require('./package')
-  const defaults = {
-    name: pkg.name,
-    version: pkg.version,
-    description: pkg.description,
-    logLevel: 'info'
-  }
-  const program = parseArguments(defaults, process.argv)
-  logger.level = program.logLevel
-
-  logger.info('%s v%s', pkg.name, pkg.version)
-  logger.info('Done!')
+  main()
 }
