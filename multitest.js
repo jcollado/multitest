103b4b35db578e6e65cdf919b40111c603cdea24
Javier Collado <javier.collado@gmail.com>
2015-12-23 14:56:45 +0100

    Add command line option parsing

--- multitest.js
+++ multitest.js
@@ -2,5 +2,19 @@
 'use strict'
 
 const logger = require('./lib/logging').logger
+const parseArguments = require('./lib/arguments').parseArguments
 
-logger.info('Done!')
+if (require.main === module) {
+  const pkg = require('./package')
+  const defaults = {
+    name: pkg.name,
+    version: pkg.version,
+    description: pkg.description,
+    logLevel: 'info'
+  }
+  const program = parseArguments(defaults, process.argv)
+  logger.level = program.logLevel
+
+  logger.info('%s v%s', pkg.name, pkg.version)
+  logger.info('Done!')
+}
