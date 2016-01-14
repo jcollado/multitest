aeeb93904cc31fd2c0c622196e833ba491bf855e
Javier Collado <javier.collado@gmail.com>
2016-01-14 15:27:22 +0100

    Add arguments test cases

---
+++ test/arguments.js
@@ -0,0 +1,34 @@
+/* global describe it */
+'use strict'
+const chai = require('chai')
+
+const expect = chai.expect
+
+describe('parseArguments', function () {
+  const parseArguments = require('../lib/arguments').parseArguments
+
+  const defaults = {
+    version: 'some version',
+    description: 'some description',
+    logLevel: 'some log level'
+  }
+
+  it('uses defaults', function () {
+    const program = parseArguments(defaults, [])
+    expect(program.version()).to.equal(defaults.version)
+    expect(program.description()).to.equal(defaults.description)
+    expect(program).to.have.property('logLevel', defaults.logLevel)
+  })
+
+  it('parses arguments as expected', function () {
+    const expected = {
+      logLevel: 'debug'
+    }
+    const program = parseArguments(
+      defaults, ['<node binary>', '<script>',
+        '-l', expected.logLevel
+      ])
+
+    expect(program).to.have.property('logLevel', expected.logLevel)
+  })
+})
