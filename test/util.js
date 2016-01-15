0ceb9937f554d3a6ec6b9f1d599ebc8918aaecbd
Javier Collado <javier.collado@gmail.com>
2016-01-15 10:48:44 +0100

    Add child_process.exec succeeds test case

--- test/util.js
+++ test/util.js
@@ -25,5 +25,19 @@ describe('exec', function () {
     return expect(util.exec('command'))
       .to.be.eventually.rejectedWith('some error')
   })
+
+  it('resolves if childProcess.exec succeeds', function () {
+    const exec = sinon.stub().yields(null, 'stdout', 'stderr')
+    const stubs = {
+      child_process: {
+        exec
+      }
+    }
+    const util = requireInject('../lib/util', stubs)
+    return expect(util.exec('command')).to.eventually.deep.equal({
+      command: 'command',
+      stdout: 'stdout',
+      stderr: 'stderr'
+    })
   })
 })
