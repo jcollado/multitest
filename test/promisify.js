9a70453952289752f9bbf23ffd04ca7ae1cd1e49
Javier Collado <javier.collado@gmail.com>
2016-01-15 15:45:16 +0100

    Move promisify to its own module

---
+++ test/promisify.js
@@ -0,0 +1,37 @@
+/* global describe it */
+'use strict'
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinon = require('sinon')
+
+chai.use(chaiAsPromised)
+
+const expect = chai.expect
+
+const promisify = require('../lib/promisify')
+
+describe('promisify', function () {
+  it('calls wrapped function with arguments', function () {
+    const fn = sinon.stub().yields()
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.be.eventually.fulfilled.then(function () {
+        expect(fn).to.have.been.calledWith('some', 'arguments')
+      })
+  })
+
+  it('rejects when wrapped function returns error', function () {
+    const fn = sinon.stub().yields('some error')
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.be.eventually.rejectedWith('some error')
+  })
+
+  it('resolves when wrapped function returns no error', function () {
+    const fn = sinon.stub().yields(null, 'a', 'result')
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.eventually.deep.equal(['a', 'result'])
+  })
+})
