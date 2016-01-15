f9489e43ee43ba2c3f453b14416870b2d5762dfc
Javier Collado <javier.collado@gmail.com>
2016-01-15 10:41:48 +0100

    Add child_process.exec failure test case

---
+++ test/util.js
@@ -0,0 +1,27 @@
+/* global describe it */
+'use strict'
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const requireInject = require('require-inject')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+require('sinon-as-promised')
+
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+
+const expect = chai.expect
+
+describe('exec', function () {
+  it('rejects if childProcess.exec fails', function () {
+    const exec = sinon.stub().yields(['some error'])
+    const stubs = {
+      child_process: {
+        exec
+      }
+    }
+    const util = requireInject('../lib/util', stubs)
+    return expect(util.exec()).to.be.eventually.rejectedWith('some error')
+  })
+})
