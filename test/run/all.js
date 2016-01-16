47720692a874cbea7c1760b527314650e77788ee
Javier Collado <javier.collado@gmail.com>
2016-01-16 06:06:46 +0100

    Add each version is tested test runAll tests test case

--- test/run/all.js
+++ test/run/all.js
@@ -17,8 +17,11 @@ const expect = chai.expect
 
 describe('runAllTests', function () {
   let logger
+  let one
   let util
   let stubs
+
+  const outputDir = '.' + pkg.name
 
   function requireModule () {
     return requireInject('../../lib/run/all', stubs)
@@ -30,12 +33,14 @@ describe('runAllTests', function () {
       info: sinon.spy(),
       error: sinon.spy()
     }
+    one = sinon.stub()
     util = {
       exists: sinon.stub(),
       mkdir: sinon.stub()
     }
     stubs = {}
     stubs[require.resolve('../../lib/logging')] = {logger}
+    stubs[require.resolve('../../lib/run/one')] = one
     stubs[require.resolve('../../lib/util')] = util
   })
 
@@ -46,10 +51,23 @@ describe('runAllTests', function () {
 
     return expect(runAllTests([])).to.eventually.be.fulfilled
       .then(function () {
-        expect(util.mkdir).to.have.been.calledWith('.' + pkg.name)
+        expect(util.mkdir).to.have.been.calledWith(outputDir)
         expect(logger.debug).to.have.been.calledWith(
-          'Directory created: %s', '.' + pkg.name)
+          'Directory created: %s', outputDir)
       })
   })
-})
 
+  it('runs tests for each version', function () {
+    util.exists.resolves()
+    one.resolves({version: 'some version', returnCode: 0})
+    const runAllTests = requireModule()
+
+    const versions = [4, 5]
+    return expect(runAllTests(versions)).to.eventually.be.fulfilled
+    .then(function () {
+      versions.forEach(function (version) {
+        expect(one).to.have.been.calledWith(outputDir, version)
+      })
+    })
+  })
+})
