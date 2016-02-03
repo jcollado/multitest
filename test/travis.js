262db99b7bfe34508cf55b7df26bcb3044ac34bf
Javier Collado <javier.collado@gmail.com>
2016-02-03 12:13:32 +0100

    Update test/travis to use ava assertions

--- test/travis.js
+++ test/travis.js
@@ -1,17 +1,9 @@
 import path from 'path'
 
-import chai from 'chai'
-import chaiAsPromised from 'chai-as-promised'
 import requireInject from 'require-inject'
 import sinon from 'sinon'
-import sinonChai from 'sinon-chai'
 import 'sinon-as-promised'
 import test from 'ava'
-
-chai.use(chaiAsPromised)
-chai.use(sinonChai)
-
-const expect = chai.expect
 
 test.beforeEach(t => {
   const stubs = {
@@ -38,9 +30,9 @@ test('travis rejects if file is not found', t => {
   const {logger, parse, util} = t.context
   util.exists = sinon.stub().rejects()
 
-  return expect(parse()).to.be.eventually.rejected.then(function () {
-    expect(logger.error).to.have.been.calledWith(
-      'Travis file not found: %s', '<travis-file>')
+  return parse().catch(() => {
+    t.true(logger.error.calledWith(
+      'Travis file not found: %s', '<travis-file>'))
   })
 })
 
@@ -49,9 +41,9 @@ test('travis rejects if language field is not found', t => {
   util.exists = sinon.stub().resolves()
   util.readFile = sinon.stub().resolves('value: 42')
 
-  return expect(parse()).to.be.eventually.rejected.then(function () {
-    expect(logger.error).to.have.been.calledWith(
-      'language field not found in travis configuration')
+  return parse().catch(() => {
+    t.true(logger.error.calledWith(
+      'language field not found in travis configuration'))
   })
 })
 
@@ -60,11 +52,11 @@ test('travis rejects if language field is not node_js', t => {
   util.exists = sinon.stub().resolves()
   util.readFile = sinon.stub().resolves('language: some-other-language')
 
-  return expect(parse()).to.be.eventually.rejected.then(function () {
-    expect(logger.error).to.have.been.calledWith(
+  return parse().catch(() => {
+    t.true(logger.error.calledWith(
       'Unexpected language in travis configuration: %s',
       'some-other-language'
-    )
+    ))
   })
 })
 
@@ -73,9 +65,9 @@ test('travis rejects if node_js field is not found', t => {
   util.exists = sinon.stub().resolves()
   util.readFile = sinon.stub().resolves('language: node_js')
 
-  return expect(parse()).to.be.eventually.rejected.then(function () {
-    expect(logger.error).to.have.been.calledWith(
-      'node_js field not found in travis configuration')
+  return parse().catch(() => {
+    t.true(logger.error.calledWith(
+      'node_js field not found in travis configuration'))
   })
 })
 
@@ -88,8 +80,9 @@ test('travis resolves to versions found', t => {
     '- 4\n' +
     '- 5')
 
-  return expect(parse()).to.eventually.deep.equal([4, 5]).then(function () {
-    expect(logger.info).to.have.been.calledWith(
-      'Node versions to use for testing: %s', [4, 5])
+  return parse().then(versions => {
+    t.same(versions, [4, 5])
+    t.true(logger.info.calledWith(
+      'Node versions to use for testing: %s', [4, 5]))
   })
 })
